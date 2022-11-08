292f4db3fa2075d0e4626d01282c666def171194
Semion <samjcool@gmail.com>
2022-11-08 11:37:28 +0200

    cv update

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href='../../assets/SCV.pdf' download className='btn'>Download CV</a>
+        <a href="#contacts" className='btn btn-primary' >Let`s Talk</a>
+    </div>
+    
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I`m </h5>
+        <h1>Semion </h1>
+        <h5 className="text-light">Full stack developer</h5>
+        <CTA/>
+      </div>
+      
+      
+    </header>
   )
 }
 
